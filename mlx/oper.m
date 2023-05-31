2879848820835883a27f2fc0242bea64ce83c6ae
Eduardo Yuji Yoshida Yamada <a2320606@vmpdc.utfpr>
2023-05-31 10:54:36 -0300

    org

---
+++ mlx/oper.m
@@ -0,0 +1,5 @@
+function [sm,pr] = oper(a,b)
+%OPER Função que realiza soma e multiplicação de duas matrizes
+sm = a+b;
+pr = a*b;
+end
--- oper.m
+++
@@ -1,5 +0,0 @@
-function [sm,pr] = oper(a,b)
-%OPER Função que realiza soma e multiplicação de duas matrizes
-sm = a+b;
-pr = a*b;
-end
